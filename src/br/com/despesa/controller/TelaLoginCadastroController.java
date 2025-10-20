423ef067aba4f2aa617618ba0971276686d3e2a0
JGuilhermeAires <jgaco@cesar.school>
2025-10-20 20:25:33 -0300

    Primeiro commit do projeto Despesas Pessoais

---
+++ src/br/com/despesa/controller/TelaLoginCadastroController.java
@@ -0,0 +1,25 @@
+package br.com.despesa.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class TelaLoginCadastroController {
+
+    @FXML
+    private Button botaoCadastro;
+
+    @FXML
+    private Button botaoEntrar;
+
+    @FXML
+    private TextField campoEmailUsuario;
+
+    @FXML
+    private TextField campoNomeUsuario;
+
+    @FXML
+    private PasswordField campoSenhaUsuario;
+
+}
