c0345e7efe763265a1ec4b7e61a7c85707768c88
JGuilhermeAires <jgaco@cesar.school>
2025-10-21 18:01:41 -0300

    Adicionando classes DAO, e telaCadastro em arquivo FXML

---
+++ src/br/com/despesa/dao/DespesaDAO.java
@@ -0,0 +1,5 @@
+package br.com.despesa.dao;
+
+public class DespesaDAO {
+    
+}
---
+++ src/br/com/despesa/dao/UsuarioDAO.java
@@ -0,0 +1,24 @@
+package br.com.despesa.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
+import br.com.despesa.factory.ConnectionFactory;
+import br.com.despesa.model.Usuario;
+
+public class UsuarioDAO {
+     public void save(Usuario usuario){
+        String sql = "INSERT INTO usuario (nome, email, senha) VALUES (?,?,?)";
+         try (Connection conn = ConnectionFactory.createConnectionToSQLServer();
+             PreparedStatement pstm = conn.prepareStatement(sql)) {
+
+            pstm.setString(1, usuario.getNome());
+            pstm.setString(2, usuario.getEmail());
+            pstm.setString(3, usuario.getSenha());
+
+            pstm.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
